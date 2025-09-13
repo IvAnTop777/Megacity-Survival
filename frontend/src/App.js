372ecf53aa3c1dd7a9ae7b2fccf6146bfe77e067
IvAnTop777 <srimplipiblz4327@gmail.com>
2025-09-13 14:55:37 +0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <Dashboard
+      resources={{ energy: 5, crystals: 123, lifeEnergy: 27 }}
+      phase={{ current: 2, total: 5, pointsToNext: 47 }}
+      biometricVerified={true}
+    />
+  );
+}
+
+export default App;
