7c558d776d535747da3b076d4652d35e69b5125c
IvAnTop777 <srimplipiblz4327@gmail.com>
2025-09-19 14:32:41 +0700

    Create ResourcesView.jsx

---
+++ frontend/src/components/Resources/ResourcesView.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ResourcesView = () => {
+  return (
+    <div style={{ color: '#fff', padding: 20 }}>
+      <h2>Resources</h2>
+      <p>⚡ Energy: 100</p>
+      <p>💎 Crystals: 50</p>
+      <p>🌆 City Stage: 1</p>
+    </div>
+  );
+};
+
+export default ResourcesView;
